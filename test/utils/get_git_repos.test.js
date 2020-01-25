27140b615c782ab38b4772c19a999250c1e12a9d
popcor255 <popcor255@gmail.com>
2020-01-25 12:16:27 -0500

    fix dirctory for get_git_repo

--- test/utils/get_git_repos.test.js
+++ test/utils/get_git_repos.test.js
@@ -1,7 +1,7 @@
 describe('Tests for the getGitRepos.js', () => {
     test('expect getGitRepos to extract array of git repos from payload', () => {
         let git_payload_mock = require('../mocks/pipeline_resources_mocks/resource_with_1_repo.json');
-        let { get_git_repo } = require('../src/utils/get_git_repos.json');
+        let { get_git_repo } = require('../../src/utils/get_git_repos.js');
         
     });
 })
