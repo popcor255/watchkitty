971833f86a87f7d71c2180331ca14dcf7e6938ff
popcor255 <popcor255@gmail.com>
2020-01-26 12:49:49 -0500

    remove test folder (redudant)

--- test/utils/get_git_repos.test.js
+++
@@ -1,8 +0,0 @@
-describe('Tests for the getGitRepos.js', () => {
-    test('expect getGitRepos to extract array of git repos from payload', () => {
-        let endpoint_response_mock = require('../mocks/pipeline_resources_mocks/resource_with_1_repo.json.js.js.js');
-        let { get_git_repos } = require('../../src/utils/get_git_repos.js');
-        
-        expect(get_git_repos(endpoint_response_mock)).toBe("get_git_repo");
-    });
-})
