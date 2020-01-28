bc1d516cb3d8f34cd7580edaffd03ff328612d25
popcor255 <popcor255@gmail.com>
2020-01-28 13:47:37 -0500

    update

--- tests/src/utils/get_git_repos.test.js
+++ tests/src/utils/get_git_repos.test.js
@@ -1,14 +1,16 @@
-var mock_root_directory = "../../mocks/";
-var src_root_directory = "../../../src/utils/";
-
+var mock_root_directory = '../../mocks/';
+var src_root_directory = '../../../src/utils/';
 describe('Tests for the getGitRepos.js', () => {
-    test('expect get_git_repos_array to a repo in an array from a json object', () => {
-        let endpoint_response_mock = require(mock_root_directory + 'pipeline_resources_mocks/resource_with_1_repo.json');
-        let { get_git_repos_as_array } = require(src_root_directory + 'get_git_repos.js');
-        expect(get_git_repos_as_array(endpoint_response_mock)).toEqual(["https://github.com/GoogleContainerTools/skaffold"]);
-    });
+	test('expect get_git_repos_array to a repo in an array from a json object', () => {
+		let endpoint_response_mock = require(mock_root_directory +
+			'pipeline_resources_mocks/resource_with_1_repo.json');
+		let { get_git_repos_as_array } = require(src_root_directory + 'get_git_repos.js');
+		expect(get_git_repos_as_array(endpoint_response_mock)).toEqual([
+			'https://github.com/GoogleContainerTools/skaffold'
+		]);
+	});
 
-    test('expect get_git_repos_map to return a map of git repos with the key being the repo and piplerun being the value', () => {
-        expect(true).toEqual(true);
-    });
-})
+	test('expect get_git_repos_map to return a map of git repos with the key being the repo and piplerun being the value', () => {
+		expect(true).toEqual(true);
+	});
+});
