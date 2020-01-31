5e36de762eb94b8dcea04d051541a18b6ee6b268
popcor255 <popcor255@gmail.com>
2020-01-31 11:03:16 -0500

    add canary

--- src/utils/parse_payload.test.js
+++ src/utils/parse_payload.test.js
@@ -1,6 +1,9 @@
 var src_file = './';
 
 describe('Tests for the parse_payload.js', () => {
+	test('canary test', () => {
+		expect(true).toBe(true);
+	});
 	test('expect payload to converted to JSON obj', () => {
 		let empty_object = '{"key":"test"}';
 		let { parse_payload } = require(src_file + 'parse_payload.js');
